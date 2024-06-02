b6d1710e46f34b9ce9dec5ef83abef3f923f868a
Sachin Bhati <69079792+Sachinbhatii@users.noreply.github.com>
2024-06-02 08:34:12 +0000

    ecma objects

---
+++ ecma/ecma_objects.js
@@ -0,0 +1,23 @@
+const demoObj = {
+    name: 'pradeep',
+    walk: function () {
+        console.log(this);
+    }
+}
+
+// demoObj.walk()
+
+const walk = demoObj.walk.bind(demoObj.walk)
+
+const carObjects = {
+    audi() {
+        console.log('audi');
+    },
+    bmw() {
+        console.log(this);
+    },
+    tata: this
+}
+
+console.log(carObjects.tata)
+carObjects.bmw()
