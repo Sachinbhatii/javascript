42dc80cff706fb057304d93c4b5094f36ae34b12
Sachin Bhati <69079792+Sachinbhatii@users.noreply.github.com>
2024-06-20 06:18:22 +0000

    destructuring

---
+++ ecma/destructuring.js
@@ -0,0 +1,14 @@
+let a, b, rest;
+[a, b] = [10, 20];
+
+console.log(a);
+
+console.log(b);
+
+[a, b, ...rest] = [10, 20, 30, 40, 50];
+
+console.log(rest);
+
+const vehicles = ['mustang', 'f-150', 'expedition'];
+const [car,, suv] = vehicles;
+console.log(suv);
